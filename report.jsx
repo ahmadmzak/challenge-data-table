d52a3c010ee608e734a791f0ccb6cc434f3141c5
Ahmed <ahmadmzak@gmail.com>
2018-11-14 14:14:56 +0500

    initial commit

--- report.jsx
+++ report.jsx
@@ -4,8 +4,84 @@ var createReactClass = require('create-react-class')
 
 var rows = require('./data.json')
 
+var dimensions = [
+    { value: 'date', title: 'Date' },
+    { value: 'host', title: 'Host' }
+]
+
+var reduce = function(row, memo) {
+    switch (row.type) {
+        case 'impression':
+            memo.impressionTotal = (memo.impressionTotal || 0) + 1
+            break
+        case 'load':
+            memo.loadTotal = (memo.loadTotal || 0) + 1
+            break
+        case 'display':
+            memo.displayTotal = (memo.displayTotal || 0) + 1
+            break
+    }
+    return memo
+}
+
+var calculations = [
+    // "value" can be the key of the "memo" object from reduce
+    // "template" changes the display of the value, but not sorting behavior
+    {
+        title: 'Impressions',
+        value: 'impressionTotal',
+        template: function(val, row) {
+            return val
+        },
+        className: 'alignRight'
+    },
+    {
+        title: 'Loads',
+        value: 'loadTotal',
+        template: function(val, row) {
+            return val
+        },
+        className: 'alignRight'
+    },
+    {
+        title: 'Displays',
+        value: 'displayTotal',
+        template: function(val, row) {
+            return val
+        },
+        className: 'alignRight'
+    },
+    {
+        title: 'Load Rate',
+        value: function(memo) {
+            return (memo.loadTotal / memo.impressionTotal) * 100
+        },
+        template: function(val, row) {
+            return val.toFixed(1) + '%'
+        },
+        className: 'alignRight'
+    },
+    {
+        title: 'Dislplay Rate',
+        value: function(memo) {
+            return (memo.displayTotal / memo.loadTotal) * 100
+        },
+        template: function(val, row) {
+            return val.toFixed(1) + '%'
+        },
+        className: 'alignRight'
+    }
+]
+
 module.exports = createReactClass({
-  render () {
-    return <div>Report</div>
-  }
+    render() {
+        return (
+            <ReactPivot
+                rows={rows}
+                dimensions={dimensions}
+                reduce={reduce}
+                calculations={calculations}
+            />
+        )
+    }
 })
